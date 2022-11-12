a315fc5e19c4eff1c7b84d667f6f5e4cbc7e577b
MathOmer <faruk774@hotmail.com>
2022-11-12 13:22:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int yil, artik;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Yıl giriniz: ");
+        yil = input.nextInt();
+
+        if (yil % 4 == 0 && yil % 100 == 0) {
+            if(yil%400==0){
+                System.out.println(yil+" artık bir yıldır.");
+            }else{
+                System.out.println(yil+" artık bir yıl değidlir.");
+            }
+        }else{
+            System.out.println(yil+" artık bir yıl değildir.");
+        }
+    }
+}
