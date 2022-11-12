5cff94448e7cab527536687bd936618c32f91998
Ömer Faruk Küçükkonuklar <109313565+MathOmer@users.noreply.github.com>
2022-11-12 13:24:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int yil, artik;
+        int yil;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Yıl giriniz: ");
